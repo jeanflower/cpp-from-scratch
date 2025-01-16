5225997457cccaf5b73fce037a150974f8b6e857
Jean Flower <jeanflower@rocketmail.com>
2025-01-16 12:04:23 +0000

    Include the boost library

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
+#include <boost/algorithm/string.hpp>
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    std::string str = "Boost C++ Libraries";
+    
+    // Convert string to lowercase using Boost
+    boost::algorithm::to_lower(str);
+
+    std::cout << "Lowercase string: " << str << std::endl;
+
+    // Check if the string contains "boost"
+    if (boost::algorithm::contains(str, "boost")) {
+        std::cout << "String contains 'boost'!" << std::endl;
+    }
+
     return 0;
 }
