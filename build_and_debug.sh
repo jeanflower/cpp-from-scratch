4a0aaa597e043f458e7dd38e25be852a72422977
Jean Flower <jeanflower@rocketmail.com>
2025-04-05 08:05:48 +0100

    Update cmake files for faster builds

--- build_and_debug.sh
+++ build_and_debug.sh
@@ -2,9 +2,9 @@
 set -e  # Exit immediately if any command fails
 
 cmake -S . -B build
-cmake -DCMAKE_BUILD_TYPE=Release -B build
-#cmake -DCMAKE_BUILD_TYPE=Debug -B build
-cmake --build build --target main
+#cmake -DCMAKE_BUILD_TYPE=Release -B build
+cmake -DCMAKE_BUILD_TYPE=Debug -B build
+cmake --build build  -j 4 --target main
 
 ./build/main
 touch ./viewer/viewer.html
