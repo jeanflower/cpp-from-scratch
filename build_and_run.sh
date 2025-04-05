4fdc5f9d20f28a21d63b6e87945c3236c3bc712d
Jean Flower <jeanflower@rocketmail.com>
2025-04-05 07:28:55 +0100

    Remove cloud work

--- build_and_run.sh
+++ build_and_run.sh
@@ -2,7 +2,6 @@
 set -e  # Exit immediately if any command fails
 
 cmake -S . -B build
-cmake -B build -DUSE_CLOUD=OFF
 cmake --build build
 
 ./build/main
--- build_docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-time docker build -t my_alpine_build .
-docker run --rm my_alpine_build
